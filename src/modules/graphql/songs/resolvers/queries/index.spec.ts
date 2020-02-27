ff434320090737656e416c193cca5c7326a7949a
Jindřich Máca <jindrich.maca@qest.cz>
2020-02-27 14:48:49 +0100

    test(song-by-id): Add tests for getting a song by its ID GQL query

--- src/modules/graphql/songs/resolvers/queries/index.spec.ts
+++ src/modules/graphql/songs/resolvers/queries/index.spec.ts
@@ -1,6 +1,17 @@
 import { createApolloTestClient } from '@container/apollo/test-hooks';
+import { SongsDataSource } from '@graphql/songs/data-sources';
 import { SONG_FRAGMENT } from '@graphql/songs/resolvers/test-hooks';
 import { gql } from 'apollo-server';
+
+const SONG_BY_ID = gql`
+    query SongById($id: ID!) {
+        song(id: $id) {
+            ...SongFragment
+        }
+    }
+
+    ${SONG_FRAGMENT}
+`;
 
 const ALL_SONGS = gql`
     query AllSongs {
@@ -23,6 +34,36 @@ const SEARCH_SONGS = gql`
 `;
 
 describe('Songs Queries', () => {
+    it('should return an error when song with given ID does not exist', async () => {
+        expect.assertions(3);
+
+        const { query } = createApolloTestClient();
+        const { errors } = await query({
+            query: SONG_BY_ID,
+            variables: { id: 'non-existing' },
+        });
+
+        expect(errors).not.toBeFalsy();
+        expect(errors).toHaveLength(1);
+        expect(errors![0].message).toMatch("Song with ID 'non-existing' does not exists.");
+    });
+
+    it('should return a song by its ID', async () => {
+        expect.assertions(1);
+
+        const songs = new SongsDataSource();
+        const allSongs = await songs.getAll();
+        const songId = allSongs[1].id;
+
+        const { query } = createApolloTestClient();
+        const { data: song } = await query({
+            query: SONG_BY_ID,
+            variables: { id: songId },
+        });
+
+        expect(song).toMatchSnapshot();
+    });
+
     it('should return all songs', async () => {
         expect.assertions(1);
 
